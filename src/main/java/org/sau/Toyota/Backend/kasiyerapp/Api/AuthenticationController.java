872a3dcb12b3f6d4f0d4d287939a23ea8ee7871a
Ahmet Alıç <ahmet.alic@ogr.sakarya.edu.tr>
2024-02-18 17:31:45 +0300

    Small changes on authentication service.

--- src/main/java/org/sau/Toyota/Backend/kasiyerapp/Api/AuthenticationController.java
+++ src/main/java/org/sau/Toyota/Backend/kasiyerapp/Api/AuthenticationController.java
@@ -1,16 +1,11 @@
 package org.sau.Toyota.Backend.kasiyerapp.Api;
 
 import lombok.RequiredArgsConstructor;
-import org.sau.Toyota.Backend.kasiyerapp.Dto.Request.UserLoginDTO;
-import org.sau.Toyota.Backend.kasiyerapp.Dto.Request.UserRegister;
+import org.sau.Toyota.Backend.kasiyerapp.Dto.Request.UserLoginRequest;
 import org.sau.Toyota.Backend.kasiyerapp.Dto.Response.TokenResponse;
-import org.sau.Toyota.Backend.kasiyerapp.Entity.User;
 import org.sau.Toyota.Backend.kasiyerapp.Service.Abstract.AuthenticationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
@@ -19,14 +14,8 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
-
-    @PostMapping("/save")
-    public ResponseEntity<TokenResponse> save(@RequestBody UserRegister userRegister){
-        return ResponseEntity.ok(authenticationService.save(userRegister));
-    }
-
     @PostMapping("/auth")
-    public ResponseEntity<TokenResponse> auth(@RequestBody UserLoginDTO userLoginDTO){
-        return ResponseEntity.ok(authenticationService.auth(userLoginDTO));
+    public ResponseEntity<TokenResponse> auth(@RequestBody UserLoginRequest userLoginRequest){
+        return ResponseEntity.ok(authenticationService.auth(userLoginRequest));
     }
 }
--- src/main/java/org/sau/Toyota/Backend/kasiyerapp/Service/Concrete/AuthenticationServiceImpl.java
+++ src/main/java/org/sau/Toyota/Backend/kasiyerapp/Service/Concrete/AuthenticationServiceImpl.java
@@ -1,24 +1,17 @@
 package org.sau.Toyota.Backend.kasiyerapp.Service.Concrete;
 
 import lombok.RequiredArgsConstructor;
-import org.sau.Toyota.Backend.kasiyerapp.Dao.RoleRepository;
 import org.sau.Toyota.Backend.kasiyerapp.Dao.UserRepository;
-import org.sau.Toyota.Backend.kasiyerapp.Dto.Request.UserLoginDTO;
-import org.sau.Toyota.Backend.kasiyerapp.Dto.Request.UserRegister;
+import org.sau.Toyota.Backend.kasiyerapp.Dto.Request.UserLoginRequest;
 import org.sau.Toyota.Backend.kasiyerapp.Dto.Response.TokenResponse;
-import org.sau.Toyota.Backend.kasiyerapp.Entity.Role;
 import org.sau.Toyota.Backend.kasiyerapp.Entity.User;
 import org.sau.Toyota.Backend.kasiyerapp.Security.JwtService;
 import org.sau.Toyota.Backend.kasiyerapp.Service.Abstract.AuthenticationService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
@@ -28,40 +21,13 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final AuthenticationManager authenticationManager;
 
-    private final PasswordEncoder passwordEncoder;
-
-    private final RoleRepository roleRepository;
-
     private final JwtService jwtService;
 
 
     @Override
-    @Transactional
-    public TokenResponse save(UserRegister userRegister) {
-        List<Role> roles = userRegister.getRoleId().stream()
-                .map(roleRepository::findById)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .collect(Collectors.toList());
-
-        User user = User.builder().name(userRegister.getName())
-                        .username(userRegister.getUsername())
-                                .password(passwordEncoder.encode(userRegister.getPassword()))
-                                        .email(userRegister.getEmail())
-                                                .roles(roles).build();
-
-
-        userRepository.save(user);
-
-        var token = jwtService.generateToken(user);
-
-        return TokenResponse.builder().token(token).build();
-    }
-
-    @Override
-    public TokenResponse auth(UserLoginDTO userLoginDTO) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLoginDTO.getUsername(), userLoginDTO.getPassword()));
-        User user = userRepository.findByUsername(userLoginDTO.getUsername()).orElseThrow();
+    public TokenResponse auth(UserLoginRequest userLoginRequest) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLoginRequest.getUsername(), userLoginRequest.getPassword()));
+        User user = userRepository.findByUsername(userLoginRequest.getUsername()).orElseThrow();
         String token = jwtService.generateToken(user);
         return TokenResponse.builder().token(token).build();
     }
